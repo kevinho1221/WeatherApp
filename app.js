30f89b350c66e9bc5c3c62561478d1007255af4a
Kevin Ho <kevinho122195@gmail.com>
2019-09-13 16:19:05 -0400

    Added in the Skycons js file to get the weather icons, made it so the the icon will match the weather, and also now allows to user to change for celsius to farenheight by clicking on the temperature icon

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    let temperatureSpan = document.querySelector('.temperature span');
+
+
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             long = position.coords.longitude;
-            lat= position.coords.latitude;
+            lat = position.coords.latitude;
 
             const proxy = 'https://cors-anywhere.herokuapp.com/';
             const api = `${proxy}https://api.darksky.net/forecast/04d5f7e6beca0852f025f8fd4303b7d6/${lat},${long}`;
@@ -15,8 +22,41 @@ window.addEventListener('load', ()=>{
                 })
                 .then (data => { 
                     console.log(data);
+                    const {temperature, summary, icon} = data.currently;
+                    
+                    //Set DOM elements from the api
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    
+                    //Formula for celsius
+                    let celcius = (temperature - 32)*(5 / 9);
+                    
+                    //set icon
+                    setIcons(icon, document.querySelector(".icon"));
+
+                    //
+                    temperatureSection.addEventListener('click', ()=>{
+                        if (temperatureSpan.textContent === "F"){
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celcius);
+                        } else{
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    })
                 })
             }
         )
     } 
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        //replace "-" with "_"
+        const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+
+
+
+    }
 });
